3b861c0beadef405dd730738316d9d5fe44fe9f2
Dima <morano747@gmail.com>
2019-06-14 10:32:48 +0300

    added ty.py

---
+++ te.py
@@ -0,0 +1,64 @@
+from selenium import webdriver
+from selenium.webdriver.chrome import service
+from selenium.webdriver.common.by import By
+
+
+
+
+print("Hello world")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# import os
+#
+# from selenium import webdriver
+# from selenium.webdriver.common import desired_capabilities
+# from selenium.webdriver.opera import options
+#
+# _operaDriverLoc = os.path.abspath('C:\\workspace\\libs\\operadriver.exe')  # Replace this path with the actual path on your machine.
+# _operaExeLoc = os.path.abspath('C:\\Users\\admin\\AppData\\Local\\Programs\\Opera\\60.0.3255.70\\opera.exe')   # Replace this path with the actual path on your machine.
+#
+# _remoteExecutor = 'http://127.0.0.1:9515'
+# _operaCaps = desired_capabilities.DesiredCapabilities.OPERA.copy()
+#
+# _operaOpts = options.ChromeOptions()
+# _operaOpts._binary_location = _operaExeLoc
+#
+# # Use the below argument if you want the Opera browser to be in the maximized state when launching.
+# # The full list of supported arguments can be found on http://peter.sh/experiments/chromium-command-line-switches/
+# _operaOpts.add_argument('--start-maximized')
+#
+# _browserDriver = webdriver.Chrome(executable_path = _operaDriverLoc, chrome_options = _operaOpts, desired_capabilities = _operaCaps)
+#
+# _operaOpts.
